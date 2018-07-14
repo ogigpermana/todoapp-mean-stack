d2791ad3c0a63e76235a95a9eb0ccfe495a031c4
Ogi G Permana <hunterdumay@gmail.com>
2018-07-15 02:14:05 +0700

    Add delete function for delete list of collection

--- routes/api/items.js
+++ routes/api/items.js
@@ -28,4 +28,15 @@ router.post('/', (req, res) => {
   newItem.save().then(item => res.json(item));
 });
 
+/**
+ * @routes      DELETE api/items/:id
+ * @description Delete a list of todo
+ * @access      Public
+**/
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success:false}));
+});
+
 module.exports = router;
